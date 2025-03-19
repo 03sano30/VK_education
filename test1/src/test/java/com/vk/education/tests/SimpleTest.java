6d21b8e619d3f53076d0709581dafcefb0eb7c95
03sano30 <8961sasha@mail.ru>
2025-03-19 13:58:19 +0300

    Написал простой автотест assertTrue(true)

---
+++ test1/src/test/java/com/vk/education/tests/SimpleTest.java
@@ -0,0 +1,10 @@
+package com.vk.education.tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class SimpleTest {
+    @Test
+    public void testTrue() {
+        assertTrue(true);
+    }
+}
